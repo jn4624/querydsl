2adf9472edc24281282044c43581701b56b5ad1d
LimJiNa <jn4624@gmail.com>
2024-07-03 22:42:53 +0900

    실무 활용 - 동적 쿼리와 성능 최적화 조회 - Where절 파라미터

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -14,6 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @Transactional
 class QuerydslApplicationTests {
+
 	@Autowired
 	EntityManager entityManager;
 
--- src/test/java/study/querydsl/repository/MemberQuerydslRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberQuerydslRepositoryTest.java
@@ -54,4 +54,36 @@ class MemberQuerydslRepositoryTest {
 
         assertThat(result).extracting("username").containsExactly("member4");
     }
+
+    @Test
+    void searchWhereParameterTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        entityManager.persist(teamA);
+        entityManager.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        entityManager.persist(member1);
+        entityManager.persist(member2);
+        entityManager.persist(member3);
+        entityManager.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+        List<MemberTeamDto> result = memberQuerydslRepository.searchByWhereParameter(condition);
+
+        for (MemberTeamDto memberTeamDto : result) {
+            System.out.println("memberTeamDto = " + memberTeamDto);
+        }
+
+        assertThat(result).extracting("username").containsExactly("member4");
+    }
 }
